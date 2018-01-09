92a61d3b35ffe6d2909b108ac9ea17f2d90bdd03
andrwkoval <35274036+andrwkoval@users.noreply.github.com>
2018-01-09 22:47:54 +0200

    Update MONSTERFORCE.cs

--- GameScripts/MONSTERFORCE.cs
+++ GameScripts/MONSTERFORCE.cs
@@ -7,9 +7,6 @@ public class MONSTERFORCE : MonoBehaviour {
 	void Update () {
  		float step = speed * Time.deltaTime;
 		transform.position = Vector3.MoveTowards (transform.position,new Vector3(0, 1.5f, 0), step);
-//		if (gameObject.transform.position == new Vector3(0, 1.5f, 0)) 
-//		{
-//			Destroy(gameObject);
-//		}
+
 	}
 }
